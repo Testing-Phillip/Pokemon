f1e615bc959b3bd367cea5aaf4f090931cb54cc3
Phillip Gooden <phillipgooden@gmail.com>
2022-06-20 14:39:05 -0500

    Java Classes & Objects
    Learning example of Classes and Objects with heavy comment.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        //declare and initialize objects
-        //classname var(object's name) = new classname() (Creates the object itself)
-        //assignment works from right to left
-        //first creates new Pokemon()
-        //next sets it equal to the Pokemon variable (p1)
-        //Var and methods are encapsulated within the object and changing the var in one object does not affect the variables in another
-        //Classes blueprints for an editable entity this case pokemon
-        //then we use that class to create objects of the class that have var and methods
+/**       declare and initialize objects
+*        classname var(object's name) = new classname() (Creates the object itself)
+*        assignment works from right to left
+*        first creates new Pokemon()
+*        next sets it equal to the Pokemon variable (p1)
+*        Var and methods are encapsulated within the object and changing the var in one object does not affect the variables in another
+*        Classes blueprints for an editable entity this case pokemon
+*        then we use that class to create objects of the class that have var and methods
+*/
         Pokemon p1 = new Pokemon();
         p1.name = "Pikachu";
         p1.level = 36;
