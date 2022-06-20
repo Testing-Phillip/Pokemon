bbf0f99285a4579bd70f7874a6f02d1903854052
Phillip Gooden <phillipgooden@gmail.com>
2022-06-20 18:32:34 -0500

    Update
    added notes on constructors

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-/**       declare and initialize objects
+/**      declare and initialize objects notes
 *        classname var(object's name) = new classname() (Creates the object itself)
 *        assignment works from right to left
 *        first creates new Pokemon()
@@ -9,21 +9,23 @@ public class Main {
 *        Classes blueprints for an editable entity this case pokemon
 *        then we use that class to create objects of the class that have var and methods
 */
-        Pokemon p1 = new Pokemon();
-        p1.name = "Pikachu";
-        p1.level = 36;
-
-        Pokemon p2 = new Pokemon();
-        p2.name = "Charmander";
-        p2.level = 70;
-
-        p2.attack();
-
-        System.out.print(p1.name+" "+p1.level);
-        System.out.print("\n"+p2.name+" "+p2.level);
-        if (p1.level<p2.level) {
-            System.out.println("\n"+p2.name + " wins");
-        }else System.out.println("\n" + p1.name + " wins");
+//        Pokemon p1 = new Pokemon();
+//        p1.name = "Pikachu";
+//
+//        Pokemon p2 = new Pokemon();
+//        p2.name = "Charmander";
+//
+//        p2.attack();
+//
+//        System.out.print(p1.name+" "+p1.level);
+//        System.out.print("\n"+p2.name+" "+p2.level);
+//        if (p1.level<p2.level) {
+//            System.out.println("\n"+p2.name + " wins");
+//        }else System.out.println("\n" + p1.name + " wins");
+//    }
+        Pokemon p1 = new Pokemon("Pikachu", 15);{//args are passed into parentheses and will represent constructors
+        System.out.print(p1.level); // ref Pokemon constructor for variables
+        }
     }
 }
 
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -2,6 +2,14 @@
 public class Pokemon {
     String name;
     int level;
+    Pokemon(){ //this is the constructor you want it to have the same name as the class
+        level = 1; // when a pokemon is created the level is set to int = 1
+        // this method will have no return type
+    }
+    Pokemon(String pName, int pLevel){
+        name = pName;//var in the class equal to the parameters that gets passed into the method
+        level = pLevel;
+    }
     void attack() {
         System.out.println(name + " attack!");
     }
